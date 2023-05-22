53a4bab1385d74db5c82053d9ec3dc9a923e2358
BbillahrariBBr <mb.billah8@gmail.com>
2023-05-22 23:43:36 +0600

    start 2nd project

--- second_project/first_app/views.py
+++ second_project/first_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def info(response):
+    return HttpResponse('This is first app info View')
+
+def house(response):
+    return HttpResponse('This is first app House View')
--- second_project/second_app/views.py
+++ second_project/second_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def contact(response):
+    return HttpResponse('This is second app Contact View')
+
+def about(response):
+    return HttpResponse('This is second app About View')
