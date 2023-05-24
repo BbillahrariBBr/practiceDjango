af7b8b4958e3cb5ea2d9fad7e2b3c68741d364af
BbillahrariBBr <mb.billah8@gmail.com>
2023-05-24 21:18:06 +0600

    module 3 done

--- second_project/first_app/views.py
+++ second_project/first_app/views.py
@@ -4,7 +4,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 def info(response):
-    return HttpResponse('This is first app info View')
+    return HttpResponse('''
+                        <h1>This is first app info View</h1>
+                        <a href= '/first_app/house'>House</a>
+                        <a href= '/second_app/about'>About</a>
+                        <a href= '/second_app/contact'>contact</a>
+                        ''')
 
 def house(response):
-    return HttpResponse('This is first app House View')
+    return HttpResponse('''
+                        <h1>This is first app House View</h1>
+                        <a href= '/first_app/info'>Info</a>
+                        <a href= '/second_app/about'>About</a>
+                        <a href= '/second_app/contact'>contact</a>
+                        ''')
--- second_project/second_app/views.py
+++ second_project/second_app/views.py
@@ -4,7 +4,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 def contact(response):
-    return HttpResponse('This is second app Contact View')
+    return HttpResponse('''
+                        <h1>This is second app Contact View</h1>
+                        <a href= '/second_app/about'>About</a>
+                        <a href= '/first_app/info'>Info</a>
+                        <a href= '/first_app/house'>House</a>
+                        ''')
 
 def about(response):
-    return HttpResponse('This is second app About View')
+    return HttpResponse('''
+                        <h1>This is second app About View</h1>
+                        <a href= '/second_app/contact'>contact</a>
+                        <a href= '/first_app/info'>Info</a>
+                        <a href= '/first_app/house'>House</a>
+                        ''')
