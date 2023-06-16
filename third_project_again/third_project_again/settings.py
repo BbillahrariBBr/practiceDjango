cf9123f7d9de1647113106705eb8208a4b416f24
BbillahrariBBr <mb.billah8@gmail.com>
2023-06-16 20:34:13 +0600

    media root or dynamic file

--- third_project_again/third_project_again/settings.py
+++ third_project_again/third_project_again/settings.py
@@ -120,6 +120,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+MEDIA_ROOT = BASE_DIR/"media"
+MEDIA_URL = 'media/'
 print(STATICFILES_DIRS)
 
 # Default primary key field type
