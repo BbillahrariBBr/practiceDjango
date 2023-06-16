49bcf101500eb4111146e91736a4a416969cd216
BbillahrariBBr <mb.billah8@gmail.com>
2023-06-16 16:26:17 +0600

    dtl for loop

--- third_project_again/first_app/views.py
+++ third_project_again/first_app/views.py
@@ -2,4 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def contact(request):
-    return render(request, './first_app/index.html',{'author':'Phitron','age':15,'marks':39,})
+    return render(request, './first_app/index.html',{'courses':[
+        {
+            'id': 1,
+            'course': 'c',
+            'teacher': 'Rahim'
+        },
+         {
+            'id': 2,
+            'course': 'c++',
+            'teacher': 'Kahim'
+        },
+         {
+            'id': 3,
+            'course': 'java',
+            'teacher': 'fahim'
+        },
+    ]})
