45df31d9f966fde7757f2be7ca9e35140b79f481
BbillahrariBBr <mb.billah8@gmail.com>
2023-06-16 17:51:15 +0600

    dtl filtering

--- third_project_again/first_app/views.py
+++ third_project_again/first_app/views.py
@@ -2,20 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 def contact(request):
-    return render(request, './first_app/index.html',{'courses':[
-        {
-            'id': 1,
-            'course': 'c',
-            'teacher': 'Rahim'
-        },
-         {
-            'id': 2,
-            'course': 'c++',
-            'teacher': 'Kahim'
-        },
-         {
-            'id': 3,
-            'course': 'java',
-            'teacher': 'fahim'
-        },
-    ]})
+    return render(request, './first_app/index.html',
+                  {"name": "I am Rahim",
+                   "marks": 86,
+                   "lst": [24,3,10,5],
+                   "blog": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente ipsam mollitia laudantium odit iusto repellendus voluptatum, reprehenderit esse illum eos! Laborum sint cum quia, voluptates repudiandae ut numquam assumenda molestias."}
+    #               {'courses':[
+    #     {
+    #         'id': 1,
+    #         'course': 'c',
+    #         'teacher': 'Rahim'
+    #     },
+    #      {
+    #         'id': 2,
+    #         'course': 'c++',
+    #         'teacher': 'Kahim'
+    #     },
+    #      {
+    #         'id': 3,
+    #         'course': 'java',
+    #         'teacher': 'fahim'
+    #     },
+    # ]}
+                  )
