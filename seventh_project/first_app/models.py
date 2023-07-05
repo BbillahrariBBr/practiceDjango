3317c90f916a7dc6e854934ad8f95943be51612c
BbillahrariBBr <mb.billah8@gmail.com>
2023-07-05 08:37:53 +0600

    9.1 Model Inheritance and Abstract base Class

--- seventh_project/first_app/models.py
+++ seventh_project/first_app/models.py
@@ -11,4 +11,28 @@ class StudentModel(models.Model):
     def __str__(self):
         return f"Name: {self.name}"
     
+    #  model inheritance -- 3 types inheritance
+    # 1. abstract base class
+    # 2. multi-table inheritance
+    # 3. proxy model
+    
+    # abstract base class
+    
+class CommonInfoClass(models.Model):
+    name = models.CharField(max_length=20)
+    roll = models.IntegerField()
+    class  Meta:
+        abstract = True
+        
+class StudentInfoModel(CommonInfoClass): 
+    roll = models.IntegerField()
+    section = models.CharField(max_length=50)
+    payment = models.IntegerField()
+    
+class TeacherInfoModel(CommonInfoClass):
+    salary = models.IntegerField()
+    
+    
+    
+    
     
